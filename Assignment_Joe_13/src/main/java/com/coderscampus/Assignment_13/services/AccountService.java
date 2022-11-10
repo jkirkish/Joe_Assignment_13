bb7ad794b68e66d391ef518be98fa2a0c8f88260
kirki <kirki@LAPTOP-7JIM99T4>
2022-11-10 03:41:16 -0500

    updated Account files for html, controller, service. Also updated users

--- Assignment_Joe_13/src/main/java/com/coderscampus/Assignment_13/services/AccountService.java
+++ Assignment_Joe_13/src/main/java/com/coderscampus/Assignment_13/services/AccountService.java
@@ -5,31 +5,50 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.stream.Stream;
 import com.coderscampus.Assignment_13.dto.Account;
 import com.coderscampus.Assignment_13.dto.Transaction;
+import com.coderscampus.Assignment_13.dto.User;
 import com.coderscampus.Assignment_13.repository.AccountRepository;
 import com.coderscampus.Assignment_13.repository.TransactionRepository;
-
+import java.util.Optional;
 @Service
 public class AccountService {
 	
 	@Autowired
 	private AccountRepository accountRepo;
 	
-	public List<Account> findAll () {
-		return accountRepo.findAll();
+	public Long addAccount(User user) {
+		Account account = new Account();
+		Integer accountNum = user.getAccounts().size() + 1;
+		account.setAccountName("Account #" + accountNum);
+		account.getUsers().add(user);
+		user.getAccounts().add(account);
+		accountRepo.save(account);
+		return account.getAccountId();
 	}
 
-	public Account saveAccount(Account account) {
-		
-		
-		return accountRepo.save(account);
+	
+
+	public Stream<Account> findAccountById(Long accountId, User user) {
+		Stream<Account> account = user.getAccounts().stream().filter(x -> x.getAccountId().equals(accountId));
+		return account;
 	}
 
-	public void delete(Long accountId) {
-		accountRepo.deleteById(accountId);
-		
+	public User saveAccount(Account account, User user) {
+		for(Account acc: user.getAccounts()) {
+			if(acc.getAccountId().equals(account.getAccountId())) {
+				acc.setAccountName(account.getAccountName());
+			}
+		}
+		return user;
+	}
+
+
+
+	public Account findById(Long accountId) {
+		Optional<Account> accountOpt = accountRepo.findById(accountId);
+		return accountOpt.orElse(new Account());
 	}
 	
 
--- Assignment_Joe_13/src/main/java/com/coderscampus/Assignment_13/web/AccountController.java
+++ Assignment_Joe_13/src/main/java/com/coderscampus/Assignment_13/web/AccountController.java
@@ -2,14 +2,13 @@ package com.coderscampus.Assignment_13.web;
 
 import java.util.List;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import java.util.stream.Stream;
 import com.coderscampus.Assignment_13.dto.Account;
 import com.coderscampus.Assignment_13.dto.Transaction;
 import com.coderscampus.Assignment_13.dto.User;
@@ -26,33 +25,26 @@ public class AccountController {
 	private UserService userService;
 	
 	@GetMapping("/users/{userId}/accounts/{accountId}")
-	public String getAccounts (ModelMap model,@PathVariable Long userId, @PathVariable Long accountId) {
+	public String getAccounts(ModelMap model, @PathVariable Long userId, @PathVariable Long accountId) {
 		User user = userService.findById(userId);
-		List<Account> accounts = accountService.findAll();
-		model.put("accounts", accounts);
-		if (accounts.size() == 1) {
-			model.put("account", accounts.iterator().next());
-		}
-		return "account";
+		Stream<Account> account = accountService.findAccountById(accountId, user);
+		Account dummyAccount = account.iterator().next();
+		model.put("user", user);
+		model.put("account", dummyAccount);
+		return "accounts";
 	}
-	@PostMapping("/account")
-	public String postCreateAccoount(Account account) {
-		System.out.println(account);
-		accountService.saveAccount(account);
-		return "redirect:/account";
+	@PostMapping("/users/{userId}/accounts")
+	public String postAccount(@PathVariable Long userId, User user) {
+		Long accountNum = accountService.addAccount(user);
+		userService.saveUser(user);
+		return "redirect:/users/" + userId + "/accounts/" + accountNum;
 	}
-	
-	
-	
-	@PostMapping("/accounts/{accountId}")
-	public String postOneAccount (Account account) {
-		accountService.saveAccount(account);
-		return "redirect:/accounts/"+account.getAccountId();
+	@PostMapping("/users/{userId}/accounts/{accountId}")
+	public String updateAccount(@PathVariable Long userId, @PathVariable Long accountId, Account account, User user) {
+		User userUpdate = accountService.saveAccount(account, user);
+		userService.saveUser(userUpdate);
+		return "redirect:/users/" + userId + "/accounts/" + accountId;
 	}
-	
-	@PostMapping("/accounts/{accountId}/delete")
-	public String deleteOneTransaction (@PathVariable Long accountId) {
-		accountService.delete(accountId);
-		return "redirect:/transactions";
-	}
+
+
 }
